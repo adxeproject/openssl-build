d76abf79255fddc5a729e4c0fef68014e7ed25e6
halx99 <halx99@live.com>
2024-12-19 23:38:04 +0800

    Fix msvc version [skip ci]

--- 1k/manifest.ps1
+++ 1k/manifest.ps1
@@ -9,4 +9,4 @@ if ($IsWin) {
 # since 3.1.60+, the llvm-19 compiling class template more strict
 $manifest['emsdk'] = '3.1.63+'
 $manifest['cmake'] = '3.29.3~3.31.1+'
-$manifest['msvc'] = '14.42'
+$manifest['msvc'] = '14.42.*'
